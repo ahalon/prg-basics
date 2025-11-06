b80d028d1636f387a6a2cd653f7816f1c5a8965d
ahalon <adam.jan.halon@gmail.com>
2025-11-06 11:12:24 +0100

    lekcja 06.11

---
+++ 04-Functions/a.py
@@ -0,0 +1,24 @@
+print("Największa liczba (max):", max(7, 5, 6, 3, 8, 2))
+
+print("Najmniejsza liczba (min):", min(4, 7, 2, 3, 9, 8))
+
+print("Długość frazy (len):", len("computer science"))
+
+print("\nZnak z klawiatury (input):")
+letter_input = input()
+print("Odczytano:", letter_input)
+print("---")
+
+string_to_convert = "20303"
+print("Liczba z ciągu (int):", int(string_to_convert))
+
+decimal_number = 304
+print("Postać binarna (bin):", bin(decimal_number))
+
+print("Postać szesnastkowa (hex):", hex(decimal_number))
+
+euro_sign = '€'
+print("Kod Unicode € (ord):", ord(euro_sign))
+
+negative_number = -17
+print("Wartość bezwzględna (abs):", abs(negative_number))
